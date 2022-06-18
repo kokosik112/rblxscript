39d6aae26a6f4e1f96304b5311772e0e481bd16e
kokosik112 <kokosiko117@gmail.com>
2022-06-18 19:59:15 +0200

    Update scrpit.lua

--- scrpit.lua
+++ scrpit.lua
@@ -1,15 +1,27 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Lumber Tycoon Gui's", HidePremium = false})
+local Window = OrionLib:MakeWindow({Name = "Lumber Tycoon Gui's", HidePremium = true, IntroEnabled =false, IntroIcon =false, IntroText =False})
 
---[[
-Name = <string> - The name of the UI.
-HidePremium = <bool> - Whether or not the user details shows Premium status or not.
-SaveConfig = <bool> - Toggles the config saving in the UI.
-ConfigFolder = <string> - The name of the folder where the configs are saved.
-IntroEnabled = <bool> - Whether or not to show the intro animation.
-IntroText = <string> - Text to show in the intro animation.
-IntroIcon = <string> - URL to the image you want to use in the intro animation.
-Icon = <string> - URL to the image you want displayed on the window.
-CloseCallback = <function> - Function to execute when the window is closed.
-]]
+local username = os.getenv('USERNAME');
+
+
+OrionLib:MakeNotification({
+	Name = "Loading Completed!",
+	Content = "Hello ",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+
+Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
